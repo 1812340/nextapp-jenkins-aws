52d9bdba924674d38a4511ec6954ee5f9afad734
waqas-ghori <waqasghori467@gmail.com>
2026-03-20 14:32:16 +0100

    CI/CD

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             today I learn, how to use jenkins and deploy on EC2
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Jenkins with EC2
+            Jenkins with EC2. 
             <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
